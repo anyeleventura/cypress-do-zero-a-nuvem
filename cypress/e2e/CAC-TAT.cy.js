db015ec19d719f3911ff448c39df9c7afeabb0f3
Anyele Ventura <124682837+anyeleventura@users.noreply.github.com>
2025-01-23 10:07:51 -0300

    adiciona testes com select, radio, checkboxes e arquivos

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -40,7 +40,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Ventura');
     cy.get('#email').type('teste@email.com');
     cy.get('#open-text-area').type(longText, { delay: 0 });
-    cy.get('#phone-checkbox').click();
+    cy.get('#phone-checkbox').check();
     cy.contains('button', 'Enviar').click();
 
     cy.get('.error').should('be.visible');
@@ -91,5 +91,59 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.fillMandatoryFieldsAndSubmit();
 
     cy.get('.success').should('be.visible');
-  });  
+  }); 
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product').select('YouTube').should('have.value', 'youtube');
+  });
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product').select('mentoria').should('have.value', 'mentoria');
+  });
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product').select(1).should('have.value', 'blog');
+  });
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]').check().should('be.checked');
+  });
+
+  it('marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]').each(typeOfService => {
+      cy.wrap(typeOfService).check().should('be.checked');
+    });
+  });
+
+  it('marca ambos checkboxes, depois desmarca o último', () => {
+    cy.get('input[type="checkbox"]').check().should('be.checked')
+    .last()
+    .uncheck()
+    .should('be.not.checked');
+  });
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('#file-upload').selectFile('cypress/fixtures/example.json')
+    .should(input => {
+      console.log(input[0].files[0].name)
+      expect(input[0].files[0].name).to.equal('example.json');
+    })
+  });
+
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload').selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+    .should(input => {
+      console.log(input[0].files[0].name)
+      expect(input[0].files[0].name).to.equal('example.json');
+    })
+  });
+
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile');
+    cy.get('#file-upload').selectFile('@sampleFile')
+    .should(input => {
+      console.log(input[0].files[0].name)
+      expect(input[0].files[0].name).to.equal('example.json');
+    });
+  });
 });
